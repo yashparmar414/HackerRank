345cb72622b4f70c0d69db6239e1dea1a3ed42d6
Yash Parmar <yashparmar414@gmail.com>
2019-10-19 15:13:17 +0530

    Add files via upload

---
+++ StairCasePyramid.java
@@ -0,0 +1,55 @@
+/*
+ * This program prints the pyramid of right-aligned-staircase
+ * https://www.hackerrank.com/challenges/plus-minus/problem
+ * Mail :- yashparmar414@gmail.com
+ * LinkedIn :- https://www.linkedin.com/in/yash-parmar-3aa06a195/
+ * Output example:
+          #
+		 ##
+		###
+	   ####
+	  #####
+	 ######
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class StairCasePyramid {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for(int i=(n-1);i>=0;i--)
+        {
+            int j;
+            for(j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=j;k<n;k++)
+            {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
