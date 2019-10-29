77c209fd9d577db589820bd5fb271f72aefde80a
Yash Parmar <yashparmar414@gmail.com>
2019-10-30 01:38:26 +0530

    Update ArrayMinMaxSum.java

--- ArrayMinMaxSum.java
+++ ArrayMinMaxSum.java
@@ -35,14 +35,8 @@ public class ArrayMinMaxSum{
 
     public static void main(String[] args) {
         int[] arr = new int[5];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < 5; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
+        for (int i = 0; i < 5; i++)
+            arr[i] = scanner.nextInt();
         miniMaxSum(arr);
         scanner.close();
     }
