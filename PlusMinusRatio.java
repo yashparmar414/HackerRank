db2dfae863a09694c78dfb1ce6d1e38ae3e3ef32
Yash Parmar <yashparmar414@gmail.com>
2019-10-30 00:29:21 +0530

    Update PlusMinusRatio.java

--- PlusMinusRatio.java
+++ PlusMinusRatio.java
@@ -16,30 +16,23 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class PlusMinusRatio {
-
+public class PlusMinusRatio{
+   
     // Complete the plusMinus function below.
     static void plusMinus(int[] arr) {
         float negativeCount=0,positiveCount=0,zeroCount=0;
         for(int i:arr)
         {
             if(i<0)
-            {
                 negativeCount+=1;
-            }
             else if(i>0)
-            {
-                positiveCount+=1;
-            }
+               positiveCount+=1;
             else
-            {
-                zeroCount+=1;
-            }
+               zeroCount+=1;
         }
         System.out.println(positiveCount/arr.length);
         System.out.println(negativeCount/arr.length);
         System.out.println(zeroCount/arr.length);
-
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -47,19 +40,14 @@ public class PlusMinusRatio {
     public static void main(String[] args) {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
         int[] arr = new int[n];
-
         String[] arrItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
         for (int i = 0; i < n; i++) {
             int arrItem = Integer.parseInt(arrItems[i]);
             arr[i] = arrItem;
         }
-
         plusMinus(arr);
-
         scanner.close();
     }
 }
