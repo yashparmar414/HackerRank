b1abb6e4eb62ee0d24a87da330da3ddaece59ff6
Yash Parmar <yashparmar414@gmail.com>
2019-10-30 00:52:49 +0530

    Update CompareTriplets.java

--- CompareTriplets.java
+++ CompareTriplets.java
@@ -1,3 +1,12 @@
+/*
+ * This program accepts two arrays, then it compares the value at index i, then increament scores according to comparison. For more details, visit below link:
+ * https://www.hackerrank.com/challenges/compare-the-triplets/problem
+ * Mail :- yashparmar414@gmail.com
+ * LinkedIn :- https://www.linkedin.com/in/yash-parmar-3aa06a195/
+ * Hackerrank :- https://www.hackerrank.com/yashparmar414/
+ * GitHub :- https://github.com/yashparmar414/
+*/
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
