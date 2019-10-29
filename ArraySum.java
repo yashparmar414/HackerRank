445c10b83fbac5fb64381805426c6f9831cf7a33
Yash Parmar <yashparmar414@gmail.com>
2019-10-30 00:45:00 +0530

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -1,3 +1,11 @@
+/*
+ * This program finds sum of all elements in array.
+ * https://www.hackerrank.com/challenges/simple-array-sum/problem
+ * Mail :- yashparmar414@gmail.com
+ * LinkedIn :- https://www.linkedin.com/in/yash-parmar-3aa06a195/
+ * Hackerrank :- https://www.hackerrank.com/yashparmar414/
+ * GitHub :- https://github.com/yashparmar414/
+*/
 import java.io.*;
 import java.math.*;
 import java.text.*;
