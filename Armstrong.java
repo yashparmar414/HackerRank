76cdf7c00f430d2a86b14bc3300d92ec91fa4948
Yash Parmar <yashparmar414@gmail.com>
2019-10-30 00:22:16 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -12,38 +12,26 @@ import java.util.*;
 public class Armstrong {
 
     public static void main(String[] args) {
-        int n;
-        int temp;
-        int cube=0;
-        int tempN;
-        int digits=0;
+        int n,temp,cube=0,tempN,digits=0;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         if(n<0)
-        {
             n=Math.abs(n);
-        }
         tempN=n;
-        while (tempN != 0) 
-        {
+        while (tempN != 0){
             digits++;
             tempN = tempN/10;
         }
         tempN=n;
-        while(tempN!=0)
-        {
+        while(tempN!=0){
             temp=tempN%10;
             temp=(int)(Math.pow(temp,digits));
             cube=cube+temp; 
             tempN=tempN/10;
         }
         if(n==cube)
-        {
             System.out.println("Number Is Armstrong");
-        }
         else
-        {
             System.out.println("Number Is Not Armstrong");
-        }
     }
 }
