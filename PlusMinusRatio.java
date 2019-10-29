6be4b71dda85416e4c9d54b53f2f31436810f333
Yash Parmar <yashparmar414@gmail.com>
2019-10-30 01:51:16 +0530

    Update PlusMinusRatio.java

--- PlusMinusRatio.java
+++ PlusMinusRatio.java
@@ -41,11 +41,8 @@ public class PlusMinusRatio{
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         int[] arr = new int[n];
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
+           arr[i] = scanner.nextInt();
         }
         plusMinus(arr);
         scanner.close();
