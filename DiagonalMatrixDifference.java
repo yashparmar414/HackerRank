bd9e006ed18ec6fd2266e7ee65455867c4ad9e41
Yash Parmar <yashparmar414@gmail.com>
2019-10-30 00:34:11 +0530

    Update and rename DiagonalSquareMatrixDifference.java to DiagonalMatrixDifference.java

--- DiagonalMatrixDifference.java
+++ DiagonalMatrixDifference.java
@@ -50,7 +50,7 @@ class Result {
     }
 }
 
-public class DiagonalSquareMatrixDifference{
+public class DiagonalMatrixDifference{
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
